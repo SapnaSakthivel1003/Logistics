24fba9f47d2464ea93ad4408e266eedfdaa18834
SapnaSakthivel1003 <sapna.s@mitrahsoft.in>
2026-06-23 10:30:40 +0530

    removed

--- src/main/java/com/CMMS/Logistics/Repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.CMMS.Logistics.Repository;
-
-import com.CMMS.Logistics.Entity.Notifications;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notifications,Long> {
-}
